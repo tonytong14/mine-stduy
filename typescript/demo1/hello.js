ed5d018025bfb48322b7c7519128cc858307fcc5
仝振 <tonytong14@163.com>
2016-06-19 18:20:02 +0800

    git 速查表

---
+++ typescript/demo1/hello.js
@@ -0,0 +1,11 @@
+var hello = (function () {
+    function hello() {
+    }
+    return hello;
+}());
+var test = (function () {
+    function test() {
+    }
+    return test;
+}());
+//# sourceMappingURL=hello.js.map
