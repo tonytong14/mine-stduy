202e02eab6298974031ceaa35fb4b111f59f2138
仝振 <tonytong14@163.com>
2016-06-19 20:33:00 +0800

    typesacript 数据类型

--- typescript/demo1/dataType.js
+++ typescript/demo1/dataType.js
@@ -6,7 +6,7 @@ var isBool = true; //布尔类型
 var isNum = 666; //number类型
 var isStr = 'sajdja'; //字符串类型
 var isArr = [1, 3, 6, 7]; // 数组类型的声明方式一
-var arr = ['sf', 'ss', '7', '8']; //数据类型声明的方式二
+var arr = ['sf', 'ss', '7', '8']; //数组类型声明的方式二
 //枚举数据类型 enum
 var Color;
 (function (Color) {
@@ -22,4 +22,18 @@ function TellEnum() {
     console.log(c);
 }
 TellEnum();
+//any 数据类型
+var anyting = '任意数据类型';
+anyting = 1213;
+anyting = [1, 3, 5];
+function tellAny() {
+    console.log(anyting);
+}
+tellAny();
+//void 数据类型 没有返回值的函数
+function VoidSting() {
+    return '10';
+}
+function other() {
+}
 //# sourceMappingURL=dataType.js.map
--- typescript/demo1/dataType.ts
+++ typescript/demo1/dataType.ts
@@ -6,7 +6,7 @@ var isBool:boolean=true; //布尔类型
 var isNum:number=666;    //number类型
 var isStr:string='sajdja'; //字符串类型
 var isArr:Array<number>=[1,3,6,7]; // 数组类型的声明方式一
-var arr:string[]=['sf','ss','7','8']; //数据类型声明的方式二
+var arr:string[]=['sf','ss','7','8']; //数组类型声明的方式二
 //枚举数据类型 enum
 enum Color {Red,Cyan,Blue};
 var colorName:string=Color[0];
@@ -16,3 +16,20 @@ function  TellEnum(){
     console.log(c);
 }
 TellEnum();
+//any 数据类型
+var anyting:any='任意数据类型';
+    anyting=1213;
+    anyting=[1,3,5];
+function  tellAny(){
+    console.log(anyting);
+}
+tellAny();
+
+//void 数据类型 没有返回值的函数
+function  VoidSting():string {
+    return '10';
+}
+
+function other():any{
+    
+}
