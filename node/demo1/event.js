125131db6d062f773a45688d669c627114a21cfa
仝振 <tonytong14@163.com>
2016-06-23 16:08:21 +0800

    eventemitter

--- node/demo1/event.js
+++ node/demo1/event.js
@@ -1,6 +1,6 @@
 // 引入 events 模块
 var events = require('events');
-var http=require('http');
+/*var http=require('http');
 // 创建 eventEmitter 对象
 var eventEmitter = new events.EventEmitter();
 // 创建事件处理程序
@@ -22,4 +22,39 @@ eventEmitter.on('data_received', function(){
 // 触发 connection 事件
 eventEmitter.emit('connection');
 
-console.log("程序执行完毕。");
+console.log("程序执行完毕。");*/
+
+//继承
+// 引用util工具方便继承
+
+var util = require ( 'util' ) ;
+
+var events = require ( 'events' ) ;
+
+// 创建自定义对象
+
+var Cat = function ( name ) {
+
+    this . name = name ;
+
+}
+
+// 继承events.EventEmitter
+
+util . inherits ( Cat , events . EventEmitter ) ;
+
+// 创建自定义对象实例
+
+var Tom = new Cat ( 'Tom' ) ;
+
+// 绑定sayHiTo事件
+
+Tom . on ( 'sayHi' , function ( someone ) {
+
+// this指向实例Tom
+
+    console . log ( this . name , " sayHiTo " , someone )
+
+} )
+
+Tom . emit ( 'sayHi' , 'jerry' )
--- node/demo1/firstApp.js
+++ node/demo1/firstApp.js
@@ -8,4 +8,5 @@ http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/plain'});
     response.end('hello word\n');
 }).listen(8888);
+
 console.log('Server running at http://127.0.0.1:8888/');
