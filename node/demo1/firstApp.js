1f8ae936aa1e8b0d373580d36ee94ab9658f234a
仝振 <tonytong14@163.com>
2016-06-27 11:35:34 +0800

    hack

--- node/demo1/firstApp.js
+++ node/demo1/firstApp.js
@@ -6,7 +6,7 @@ http.createServer(function(request,response){
     //http状态值
     //内容类型：text/plain
     response.writeHead(200,{'Content-Type':'text/plain'});
-    response.end('hello word\n');
+    response.end('hello word 闫\n');
 }).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888/');
