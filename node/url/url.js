ed220339e0866dd8eba635b13dc1bb9b5c25c1d8
仝振 <tonytong14@163.com>
2016-06-30 20:56:11 +0800

    url

---
+++ node/url/url.js
@@ -0,0 +1,13 @@
+//url模块（资源定位符模块）
+    //url的构成
+    /*
+                                href
+        -----------------------------------------------------------------
+                                       host              path
+                                 --------------- ----------------------------
+            http: // user:pass @ host.com : 8080 /p/a/t/h ?query=string #hash
+            -----    ---------   --------   ---- -------- ------------- -----
+            protocol     auth     hostname   port pathname     search     hash
+                                                           ------------
+                                                                query
+    */
