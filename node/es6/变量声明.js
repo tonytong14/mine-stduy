34fcf2b6bd4980f25414ac180117c22e3612b7c0
仝振 <tonytong14@163.com>
2016-07-14 14:26:52 +0800

    es6

---
+++ node/es6/变量声明.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tongzhen on 2016/7/14.
+ */
+{
+    let parent='this is parent';
+    {
+        let child='this is child';
+    }
+
+    console.log(parent);
+    console.log(child);
+}
+
+//console.log(jubu);
